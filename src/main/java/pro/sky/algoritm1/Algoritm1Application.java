9f2d3640795132f5b1d1f7b199710453eecdc2f8
User <git@github.com:gdsStern/HelloWorld.git>
2024-07-08 16:17:49 +0300

    Files

---
+++ src/main/java/pro/sky/algoritm1/Algoritm1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.algoritm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algoritm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Algoritm1Application.class, args);
+	}
+
+}
